6d554bdf96b5480169dd78d6585d4d91fcc012e0
Rath Panyowat <rathjungzz@hotmail.com>
2015-06-26 20:18:49 +0700

    End Lesson 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,69 @@
-$("#main").append("Rath Panyowat");
+
+var bio = {
+	"name" : "Rath Panyowat",
+	"role" : "Front-end Web Developer",
+	"contacts" : {
+		"mobile" : "+6689-422-5559",
+		"email" : "rathjungzz@hotmail.com",
+		"twitter" : "@rathjung",
+		"github" : "rathjung",
+		"blog" : "rath.asia",
+		"location" : "Bangkok, Thailand"
+	},
+	"bioPic" : "images/avatar.jpg",
+	"welcomeMsg" : "Hi, Welcome to my portfolio.",
+	"skills" : ["awesomeness","JavaScript","HTML","CSS"]
+}
+
+var projects = {
+	"projects" : [
+		{
+			"title" : "test",
+			"dates" : "test",
+			"description" : "test",
+			"images" : ["",""]
+		}
+	]
+}
+
+var work = {
+	"jobs" : [
+		{
+			"employer" : "Fang Hospital",
+			"title" : "General Practitioner",
+			"dates" : "2011",
+			"location" : "Chiang Mai, Thailand",
+			"description" : "Provide a medical services to patients around Fang District."
+		}
+	]
+}
+
+var education = {
+	"schools" : [
+		{
+			"name" : "Faculty of Medicine, Khon Kaen University",
+			"degree" : "Doctor of Medicine",
+			"dates" : "2011",
+			"location" : "Khon Kaen, Thailand",
+			"major" : ["medicine"],
+			"url" : "test"
+		},
+		{
+			"name" : "Demonstration School, Khon Kaen University",
+			"degree" : "Secondary School",
+			"dates" : "2006",
+			"location" : "Khon Kaen, Thailand",
+			"major" : ["none"],
+			"url" : "test"
+		}
+	],
+	"onlineCourses" : {
+		"title" : "Test",
+		"school" : "Test",
+		"dates" : "test",
+		"url" : "url"
+	}
+}
+
+
+
